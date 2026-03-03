b472caadbda925c440ebf9ede0b828460f03b970
harshaavardhinirajaguru-afk <harshaavardhinirajaguru@gmail.com>
2026-03-03 04:07:03 +0000

    Coded UC5-PrintOOPS

---
+++ UC5.java
@@ -0,0 +1,24 @@
+public class UC5{
+    //Main method to run the banner display
+    public static void main(String[]args){
+        //Define string array variable to hold the oops banner lines
+        //Each line represents a row in the banner for the letters O,O,P,S
+            String[] lines = {
+                String.join("","    ***   ", "   *** " ," ******    " ,"   *****  "),
+                String.join(""," **     **", " **   **"," **    **  " ,"  **      "),
+                String.join(""," **     **", " **   **"," **      **" ," **       "),
+                String.join(""," **     **", " **   **"," **    **  " ,"  **      "),
+                String.join(""," **     **", " **   **"," ******    " ,"    ***   "),
+                String.join(""," **     **", " **   **"," **        " ,"       ** "),
+                String.join(""," **     **", " **   **"," **        " ,"        **"),
+                String.join(""," **     **", " **   **"," **        " ,"       ** "),
+                String.join("","    ***   ", "   ***  "," **        " ,"  *****   "),
+            };
+
+            //Use a loop to print each line of the banner to create the
+            //visual effect for the message"OOPS"
+            for(String line:lines){
+                System.out.println(line);
+            }
+    }
+}
