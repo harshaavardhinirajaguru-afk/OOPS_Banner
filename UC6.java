5e5c045f1b297f645b04a03b7dfb8a2c206f4bb2
harshaavardhinirajaguru-afk <harshaavardhinirajaguru@gmail.com>
2026-03-10 14:09:37 +0000

    Coded UC6

---
+++ UC6.java
@@ -0,0 +1,55 @@
+public class UC6{
+    public static String[] getOPattern(){
+        return new String[]{
+            "      ***         ",
+            "   **     **       ",
+            "  **       **      ",
+            "  **       **      ",
+            "  **       **      ",
+            "   **     **       ",
+            "      ***          ",
+        };
+    public static String[] getOPattern(){    
+        return new String[]{
+            "      ***          ",
+            "   **     **       ",
+            "  **       **      ",
+            "  **       **      ",
+            "  **       **      ",
+            "   **     **       ",
+            "      ***          ",
+        };
+    public static String[] getPPattern(){    
+        return new String[]{
+            " *******       ",
+            " **     **     ",
+            " **       **   ",
+            " **     **     ",
+            " **    **      ",
+            " ******        ",
+            " **            ",
+            " **            ",
+            " **            ",
+    };
+    public staic String[] getSPattern(){
+        return new String[]{
+            "      *****      ",
+            "   **            ",
+            " **              ",
+            "  **             ",
+            "     ***         ",
+            "        **       ",
+            "          **     ",
+            "         **      ",
+            "    ******       ",
+        };
+        String[]oPattern=getOPattern();
+        String[]oPattern=getOPattern();
+        String[]pPattern=getPPattern();
+        String[]sPattern=getSPattern();
+        for(int i=0;i,oPattern.length;i++){
+            System.out.println(oPattern[i]+""+oPattern[i]+""+pPattern[i]+""+sPattern[i]);
+        }
+    }
+}
+    
